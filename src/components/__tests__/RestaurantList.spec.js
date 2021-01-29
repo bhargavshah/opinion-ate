394557273ae559fe14bd2de9c44b265ec291e64b
Bhargav Shah <bhargav3shah@gmail.com>
2021-01-29 20:28:52 +0530

    Refactor RestaurantList unit tests

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -1,33 +1,36 @@
 import {render} from '@testing-library/react';
 import RestaurantList from '../RestaurantList';
 
-describe('RestauntList', () => {
-  it('should render list of restaurants', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
+describe('RestaurantList', () => {
+  const restaurants = [
+    {
+      id: 1,
+      name: 'Pasta Place',
+    },
+    {
+      id: 2,
+      name: 'Sushi Place',
+    },
+  ];
+  let loadRestaurants;
+  let context;
 
-    render(
-      <RestaurantList loadRestaurants={loadRestaurants} restaurants={[]} />,
+  beforeEach(() => {
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
+    context = render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
     );
+  });
 
+  it('should render list of restaurants', () => {
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('displays the restaurants', () => {
-    const noop = () => {};
-    const restaurants = [
-      {
-        id: 1,
-        name: 'Pasta Place',
-      },
-      {
-        id: 2,
-        name: 'Sushi Place',
-      },
-    ];
-
-    const {queryByText} = render(
-      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
-    );
+    const {queryByText} = context;
 
     expect(queryByText('Pasta Place')).not.toBeNull();
     expect(queryByText('Sushi Place')).not.toBeNull();
