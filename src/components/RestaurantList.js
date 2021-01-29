f0efbf432f59a7592781e9e639efd1b884698f10
Bhargav Shah <bhargav3shah@gmail.com>
2021-01-29 19:30:42 +0530

    Add RestaurantScreen and RestaurantList components

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,5 @@
+function RestaurantList() {
+  return 'Restaurant list';
+}
+
+export default RestaurantList;
---
+++ src/components/RestaurantScreen.js
@@ -0,0 +1,12 @@
+import RestaurantList from './RestaurantList';
+
+export const RestaurantScreen = () => {
+  return (
+    <>
+      <h1>Restaurant screen</h1>
+      <RestaurantList />
+    </>
+  );
+};
+
+export default RestaurantScreen;
